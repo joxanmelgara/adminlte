34bb7ede4d6427a6653ff7d5e950d7c06b9f1a22
joxanmelgara <joxan2018@outlook.com>
2020-11-01 10:12:25 -0600

    Update Config.php

--- config/Config.php
+++ config/Config.php
@@ -1,6 +1,6 @@
 <?php
 
-define('BASE_URL','http://localhost/hxz/adminlte/');
+define('BASE_URL','http://localhost/adminlte/');
 define('DEFAULT_CONTROLLER','index');
 define('PLANTILLA',BASE_URL.'Views/plantilla/');
 
