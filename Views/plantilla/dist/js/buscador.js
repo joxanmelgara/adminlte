6c4d8aaafc542586254a4bafb84b20e6baf8065c
joxanmelgara <joxan2018@outlook.com>
2020-11-30 07:39:58 -0600

    Delete buscador.js

--- Views/plantilla/dist/js/buscador.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-	$('#busqueda').keyup(function () {
-		var resultado = new RegExp($(this).val());
-		$('.busquedatabla tr').hide();
-		$('.busquedatabla tr').filter(function () {
-			return resultado.test($(this).text());
-		}).show();
-	})
-});
